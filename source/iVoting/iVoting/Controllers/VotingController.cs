aa17e6d62100060ec1092045163776b462fd317a
Kevin Tan <kevintan1983@hotmail.com>
2015-09-23 01:12:46 +0800

    Initial

---
+++ source/iVoting/iVoting/Controllers/VotingController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using iVoting.Models;
+
+namespace iVoting.Controllers
+{
+    public class VotingController : Controller
+    {
+        private VotingDBContext db = new VotingDBContext();
+
+        // GET: /Voting/
+        public ActionResult Index()
+        {
+            return View(db.Votings.ToList());
+        }
+
+        // GET: /Voting/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VotingModel votingmodel = db.Votings.Find(id);
+            if (votingmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(votingmodel);
+        }
+
+        // GET: /Voting/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Voting/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Gender,Exciting,Happy,Sad,Upset")] VotingModel votingmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Votings.Add(votingmodel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(votingmodel);
+        }
+
+        // GET: /Voting/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VotingModel votingmodel = db.Votings.Find(id);
+            if (votingmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(votingmodel);
+        }
+
+        // POST: /Voting/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Gender,Exciting,Happy,Sad,Upset")] VotingModel votingmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(votingmodel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(votingmodel);
+        }
+
+        // GET: /Voting/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VotingModel votingmodel = db.Votings.Find(id);
+            if (votingmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(votingmodel);
+        }
+
+        // POST: /Voting/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VotingModel votingmodel = db.Votings.Find(id);
+            db.Votings.Remove(votingmodel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ source/iVoting/iVoting/Models/VotingModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iVoting.Models
+{
+    public class VotingModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "我是")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "很興奮")]
+        public int Exciting { get; set; }
+
+        [Required]
+        [Display(Name = "好開心")]
+        public int Happy { get; set; }
+
+        [Required]
+        [Display(Name = "很難過")]
+        public int Sad { get; set; }
+
+        [Required]
+        [Display(Name = "好生氣")]
+        public int Upset { get; set; }
+    }
+
+    public enum Emotion
+    {
+        Exciting = 0,
+        Happy = 1,
+        Sad = 2,
+        Upset = 3
+    }
+
+    public class VotingDBContext : DbContext
+    {
+        public DbSet<VotingModel> Votings { get; set; }
+    }
+}
