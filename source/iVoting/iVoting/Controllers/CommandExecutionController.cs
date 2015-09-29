c1bfaa8e3e40c5786ce76d935d1090c57d0ff950
Kevin Tan <kevintan@yahoo-inc.com>
2015-09-29 22:19:57 +0800

    增加執行命令的功能，command位置透過appsetting裡面「ConsoleCommand」去定義

---
+++ source/iVoting/iVoting/Controllers/CommandExecutionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iVoting.Controllers
+{
+    public class CommandExecutionController : Controller
+    {
+		[HttpGet]
+		public ActionResult ExecuteConsole(string id)
+        {
+			this.ExecuteCommand(id);
+            return View();
+        }
+
+		private void ExecuteCommand(string argument)
+		{
+			ProcessStartInfo ProcessInfo;
+			Process Process;
+			var commandLocation = ConfigurationManager.AppSettings["ConsoleCommand"];
+
+			ProcessInfo = new ProcessStartInfo("cmd.exe", "/K " + commandLocation + " " + argument);
+			ProcessInfo.CreateNoWindow = false;
+			ProcessInfo.UseShellExecute = true;
+
+			Process = Process.Start(ProcessInfo);
+		}
+    }
+}
